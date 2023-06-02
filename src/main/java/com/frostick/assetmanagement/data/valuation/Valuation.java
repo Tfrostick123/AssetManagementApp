93f61996f12df4211d0964e600dda29cefeb307d
Tfrostick123 <tomfrostick@hotmail.co.uk>
2023-06-02 18:02:25 +0100

    missed commit

--- src/main/java/com/frostick/assetmanagement/data/valuation/Valuation.java
+++ src/main/java/com/frostick/assetmanagement/data/valuation/Valuation.java
@@ -1,2 +1,34 @@
-package com.frostick.assetmanagement.data.valuation;public interface Valuation {
+package com.frostick.assetmanagement.data.valuation;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+/**
+ * Interface defining Valuation object.
+ */
+public interface Valuation {
+    /**
+     * The date.
+     * @return the date
+     */
+    LocalDate getDate();
+
+    /**
+     * Set the date.
+     * @param date the date
+     */
+    void setDate(LocalDate date);
+
+    /**
+     *
+     * Get the value.
+     * @return the value
+     */
+    double getValue();
+
+    /**
+     * Set the value.
+     * @param value the value
+     */
+    void setValue(double value);
 }
